14dbffdca3f109c0b4501a1985739cc3c830a13a
Jonathan Tuzman <tuzmusic@gmail.com>
2023-08-11 13:38:00 -0400

    initial fixture traversal

--- src/start.ts
+++ src/start.ts
@@ -1,35 +1,56 @@
 import * as fs from 'fs';
 import path from 'path';
-import {ast as astParser, match, query} from '@phenomnomnominal/tsquery';
+import * as tsQuery from '@phenomnomnominal/tsquery';
 
 console.log('App running');
 
+const defaultValues = {
+  boolean: false,
+  number: 42,
+  string: 'abcdefg',
+};
+
 export function parse(filePath: string) {
+  const { ...q } = tsQuery;
   const text = fs.readFileSync(path.resolve(__dirname, filePath), 'utf8');
 
-  const ast = astParser(text);
-  const $ = (selector: string, tree = ast) => query(tree, selector);
+  const ast = tsQuery.ast(text);
 
-  const [schemas] = query(ast, 'PropertySignature:has(Identifier[name="schemas"])');
-  // const allSchemas = query(schemas!, 'PropertySignature');
-  const allSchemas = query(
-    ast,
-    'PropertySignature:has(Identifier[name="schemas"])' + ' > TypeLiteral > PropertySignature'
-  );
+  const [schemaTree] = tsQuery.query(ast, 'PropertySignature:has(Identifier[name="schemas"]) > TypeLiteral');
 
-  // 12 types that aren't enums?
-  const typeLiterals = query(schemas!, 'TypeLiteral'); // length = 12
+  if (!schemaTree) {
+    throw "Couldn't find node called 'schemas'";
+  }
 
-  const first = typeLiterals[0]!;
-  // typeLiterals[0].members.length = 16
-  //                .members[0].name = BillingInformation
-  //                .members[1].name = Customer
-  //
-  // typeLiterals[0].children[1].children[0].name = BillingInformation
+  // @ts-ignore (there is definitely a property called members. Debugger gets the type right but TS gets it wrong???
+  const all16Schemas = schemaTree.members;
+  const allFixtures: Record<string, any> = {};
+  all16Schemas.forEach(type => {
+    console.log(type);
+    const typeName = type.name.escapedText;
+    const fixture: Record<string, (typeof defaultValues)[keyof typeof defaultValues]> = {};
 
-  // typeLiterals[1].members[0].name = addressLineOne ( = BillingInformation.addressLineOne
+    const properties = type.type.members;
+    if (!properties) {
+      console.log(`Hm, no properties for ${typeName}`);
+      return;
+    }
+    properties.forEach(property => {
+      console.log(property);
+      const propName = property.name.escapedText;
+      const propType: keyof typeof defaultValues = property.type.getText().split(' | ')[0];
 
-  console.log(schemas);
+      if (defaultValues[propType]) {
+        fixture[propName] = defaultValues[propType];
+      } else {
+        fixture[propName] = `TBD (no default value for "${propType}")`;
+      }
+    });
+
+    allFixtures[typeName] = fixture;
+  });
+
+  console.log(allFixtures);
 }
 
 parse('../fixtures/index.d.ts');
