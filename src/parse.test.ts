a6f9adefd652a7921ce0bcd22a6b54d0831d9ab1
Jonathan Tuzman <tuzmusic@gmail.com>
2023-08-11 14:24:38 -0400

    flesh out tests

--- src/parse.test.ts
+++ src/parse.test.ts
@@ -1,9 +1,53 @@
-import {describe, expect, it} from 'vitest';
-import {parse} from './start.js';
+import {describe, expect, it, test} from 'vitest';
+import {defaultValues, parse} from './start.js';
+import type {components} from '../fixtures/index.js';
 
 describe('Parsing', () => {
-  it('parses, I guess', () => {
-    const text = parse('../fixtures/index.d.ts');
-    expect(text).toBeTruthy();
+  const fixture = parse('../fixtures/index.d.ts') as components['schemas'];
+  describe('basic types', () => {
+    it('Handles simple strings', () => {
+      expect(fixture.BillingInformation.addressLineOne).toEqual(defaultValues.string);
+      expect(fixture.Customer.emailAddress).toEqual(defaultValues.string);
+    });
+
+    it('handles "string | null"', () => {
+      expect(fixture.BillingInformation.state).toEqual(defaultValues.string);
+      expect(fixture.Customer.phoneNumber).toEqual(defaultValues.string);
+    });
+
+    it('handles numbers', () => {
+      expect(fixture.SubscriptionLine.quantity).toEqual(defaultValues.number);
+      expect(fixture.Customer.id).toEqual(defaultValues.number);
+      expect(fixture.Customer.shopifyCustomerId).toEqual(defaultValues.number);
+    });
+
+    it('handles "number | null"', () => {
+      expect(fixture.BaseSubscriptionLineLifecycle.maximumAllowedOrders).toEqual(defaultValues.number);
+      expect(fixture.Subscription.shopifySubscriptionContractId).toEqual(defaultValues.number);
+    });
+
+    it('handles booleans', () => {
+      expect(fixture.ShipZone.noSaturday).toEqual(false);
+    });
+
+    describe('inline enums', () => {
+      test('strings', () => {
+        expect(fixture.Customer.paymentGateway).toEqual('stripe');
+      });
+
+      test.todo('numbers', () => {});
+
+      test.todo('other???');
+    });
+
+    it.todo('handles null/undefined');
   });
+
+  describe.todo('single-level nested types (reference types)', () => {});
+
+  describe.todo('using examples', () => {});
+
+  describe.todo('complex types', () => {});
+
+  describe.todo('enums', () => {});
 });
